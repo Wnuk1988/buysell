907fda2b3d60bc9784b535b5d5b98f90764b0aad
Михаил <Wnuk1988@mail.ru>
2023-07-26 11:02:15 +0300

    Initial commit!

---
+++ src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,14 @@
+package com.example.buysell.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String title;
+    private String description;
+    private Integer price;
+    private String city;
+    private String author;
+}
